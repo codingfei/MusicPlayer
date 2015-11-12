a061620225ee47d39cb6ea12d50ec04786fb56f6
SidaLiu <lsdnewstar@163.com>
2015-11-12 17:44:53 +0800

    2015-11-12-刘思达
    主界面布局

--- gen/com/ckt/musicplayer/R.java
+++ gen/com/ckt/musicplayer/R.java
@@ -21,10 +21,47 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_playback_ic_play_small=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_menu_clear_playlist=0x7f020002;
+        public static final int ic_menu_eq=0x7f020003;
+        public static final int ic_menu_shuffle=0x7f020004;
+        public static final int ic_mp_current_playlist_btn=0x7f020005;
+        public static final int ic_widget_repeat_all_btn_normal=0x7f020006;
+        public static final int ic_widget_repeat_all_btn_press=0x7f020007;
+        public static final int ic_widget_repeat_once_btn_normal=0x7f020008;
+        public static final int ic_widget_repeat_once_btn_press=0x7f020009;
+        public static final int ic_widget_shuffle_normal=0x7f02000a;
+        public static final int ic_widget_shuffle_press=0x7f02000b;
+        public static final int widget_music_btn_next_normal=0x7f02000c;
+        public static final int widget_music_btn_next_press=0x7f02000d;
+        public static final int widget_music_btn_pause_normal=0x7f02000e;
+        public static final int widget_music_btn_pause_press=0x7f02000f;
+        public static final int widget_music_btn_play_normal=0x7f020010;
+        public static final int widget_music_btn_play_press=0x7f020011;
+        public static final int widget_music_btn_pre_normal=0x7f020012;
+        public static final int widget_music_btn_pre_press=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int CD_img=0x7f080002;
+        public static final int action_settings=0x7f080012;
+        public static final int btn_layout=0x7f080009;
+        public static final int buttom_layout=0x7f080003;
+        public static final int info_layout=0x7f080005;
+        public static final int list_btn=0x7f08000f;
+        public static final int main_layout=0x7f080000;
+        public static final int menu_btn=0x7f080011;
+        public static final int menu_layout=0x7f08000d;
+        public static final int music_CD=0x7f080007;
+        public static final int music_name=0x7f080006;
+        public static final int music_progerss=0x7f080004;
+        public static final int music_singer=0x7f080008;
+        public static final int next_btn=0x7f08000b;
+        public static final int play_btn=0x7f08000a;
+        public static final int pre_btn=0x7f08000c;
+        public static final int rand_btn=0x7f080010;
+        public static final int top_layout=0x7f080001;
+        public static final int voice_seekbar=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +72,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int music_CD=0x7f050003;
+        public static final int music_name=0x7f050002;
+        public static final int music_singer=0x7f050004;
     }
     public static final class style {
         /** 
